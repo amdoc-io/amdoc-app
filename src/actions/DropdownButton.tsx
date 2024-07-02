1b7cc83caa80f22338ad4d64bc0a4ee8b9993e45
Tung Dinh <tungxd301@gmail.com>
2024-07-01 21:11:02 -0700

    Changes

--- src/actions/DropdownButton.tsx
+++ src/actions/DropdownButton.tsx
@@ -27,7 +27,7 @@ export default function DropdownButton(props: {
         <MenuButton
           className={`inline-flex justify-between items-center w-full gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ${
             variant === "blank"
-              ? "ring-0 py-0 px-0 gap-x-0 hover:bg-white shadow-none"
+              ? "!ring-0 !py-0 !px-0 !gap-x-0 hover:!bg-white !shadow-none"
               : ""
           }`}
         >
